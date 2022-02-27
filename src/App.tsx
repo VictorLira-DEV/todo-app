0cb3491afa75988303f31ff51fa160c0b282b1c2
VictorLira-DEV <victorliradev@gmail.com>
2022-02-27 19:25:04 -0300

    get started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  Navbar,
+  Nav,
+  NavbarBrand,
+  Container,
+  NavItem,
+  NavLink,
+  InputGroup,
+  Button,
+  Input
+} from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="navbar-expand-md bg-dark navbar-dark py-3 ">
+        <Container className="d-flex align-itens-center justify-content-between">
+          <NavbarBrand href="#" className="navbar-brand">
+            Front-end Bootcamp
+          </NavbarBrand>
+          <Nav className="d-flex">
+            <NavItem>
+              <NavLink className="text-light" href="#">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="text-light" href="#">
+                Contacts
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="text-light" href="#">
+                About
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Container>
+      </Navbar>
+      <main className="min-vh-100 d-flex align-items-center justify-content-center bg-dark">
+        <Container className="bg-light p-5 ">
+          <div className="d-flex flex-column align-items-center">
+            <div>
+              <h1>My Tasks</h1>
+            </div>
+            <InputGroup className="w-50 my-5">
+              <Input
+                type="text"
+                className="form-control"
+                placeholder="Add a new task"
+                aria-label="Recipient's username"
+                aria-describedby="button-addon2"
+              />
+              <Button className="btn w-5" type="button" id="button-addon2">
+                Button
+              </Button>
+            </InputGroup>
+          </div>
+          <div>
+            <h1 className="mb-5">List Of Tasks</h1>
+            <Nav className="list-group w-50 m-auto">
+              <NavItem className="list-group-item ">An item</NavItem>
+              <NavItem className="list-group-item">A second item</NavItem>
+              <NavItem className="list-group-item ">An item</NavItem>
+              <NavItem className="list-group-item">A second item</NavItem>
+            </Nav>
+          </div>
+        </Container>
+      </main>
     </div>
   );
 }
